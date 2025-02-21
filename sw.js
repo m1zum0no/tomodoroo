6dbec28ee700903f63b2d78aff3249f20a7378db
Slyvia Adriene <slyviaad@student.21-school.ru>
2025-02-21 19:11:23 +0300

    modified sw paths

--- sw.js
+++ sw.js
@@ -1,28 +1,31 @@
 const cacheName = "clock";
 const contentToCache = [
-	"./",
-	"../../index.html",
-	"../../style.css",
-	
-	"../main.js",
-	"../timer.js",
-	"../statistics.js",
-	"./sw.js",
-	"./view.js",
-	"./worker.js",
-	"../profile_settings/audio.js",
-	"../profile_settings/notifications.js",
-	"../profile_settings/theming.js",
-	"../profile_settings/timer_duration.js",
+	"/",
+	"/index.html", 
+	"/style.css",
 
-	"../../meta/icon192.png",
-	"../../meta/icon512.png",
-	"../../meta/maskable192.png",
-	"../../meta/maskable512.png",
-	"../../meta/favicon.png",
-	"../../meta/appletouch.png",
-	"../../meta/GitHub-Mark-64px.png"
+	"/sw.js", 
+	"/scripts/main.js",
+	"/scripts/timer.js",
+	"/scripts/statistics.js",
+
+	"/scripts/utils/worker.js",
+	"/scripts/utils/view.js",
+
+	"/scripts/profile_settings/audio.js",
+	"/scripts/profile_settings/notifications.js",
+	"/scripts/profile_settings/theming.js",
+	"/scripts/profile_settings/timer_duration.js",
+	
+	"/meta/icon192.png",
+	"/meta/icon512.png",
+	"/meta/maskable192.png",
+	"/meta/maskable512.png",
+	"/meta/favicon.png",
+	"/meta/appletouch.png",
+	"/meta/GitHub-Mark-64px.png"
 ];
+
 
 self.addEventListener("install", (e) => {
 	console.log("Service Worker installed");
